cddfe0166aee96c0c7d77cf21e04a05fbc204c56
João Neto <j.alexandreeneto@gmail.com>
2017-12-01 21:50:47 -0300

    Project bootstrap

---
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+
+<title>Hello World</title>
+
+</head>
+
+<body>
+
+<h3>Hello World - Summer Week</h3>
+
+</body>
+
+</html>
---
+++ src/test/java/br/upe/summerweek/SummerweekApplicationTests.java
@@ -0,0 +1,16 @@
+package br.upe.summerweek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SummerweekApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
