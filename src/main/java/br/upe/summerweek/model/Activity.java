cd4e1dc1b4c25dd25e3e25c1ad2a85328ae9318b
Matheus-felipe <matheus0906.mhci@gmail.com>
2017-12-05 11:29:58 -0300

    Activity class created

---
+++ src/main/java/br/upe/summerweek/model/Activity.java
@@ -0,0 +1,60 @@
+package br.upe.summerweek.model;
+
+import java.util.Date;
+
+public class Activity {
+	private String type;
+	private String title;
+	private String description;
+	private String name; /*Palestrante*/
+	private Date date;
+	private float hours;
+
+	public void setName(String pName){
+		this.name = pName;
+	}
+	
+	public void setType(String ptype){
+		this.type = pType;
+	}
+
+	public void setTitle(String pTitle){
+		this.title = pTitle;
+	}
+	
+	public void setDescription(String pDescription){
+		this.description = pDescription;
+	}
+	
+	public void setDate(Date pDate){
+		this.date = pDate;
+	}
+	
+	public void setHours(float pHours){
+		this.hours = pHours;
+	}
+
+	public String getType(){
+		return this.type;
+	}
+	
+	public String getTitle(){
+		return this.title;
+	}	
+
+	public String getDescription(){
+		return this.description;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public Date getDate(){
+		return this.date;
+	}
+
+	public float getHours(){
+		return this.hours;
+	}
+} 
