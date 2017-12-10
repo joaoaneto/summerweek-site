02878c3a792d845719bfb0903ae23a1d18f9e78f
Matheus-felipe <matheus0906.mhci@gmail.com>
2017-12-10 01:38:47 -0300

    CARAI DE ASA

--- src/main/java/br/upe/summerweek/model/Activity.java
+++ src/main/java/br/upe/summerweek/model/Activity.java
@@ -1,19 +1,70 @@
 package br.upe.summerweek.model;
 
-import java.util.Date;
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.Calendar;
 
-public class Activity {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+@Entity
+@Table (name = "activity")
+public class Activity implements Serializable{
+
+	private static final long serialVersionUID = -3009157732242241606L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(name = "type")
 	private String type;
+
+	@Column(name = "title")
 	private String title;
+
+	@Column(name = "description")
 	private String description;
-	private String name; /*Palestrante*/
-	private Date date;
+
+	@Column(name = "speakername")
+	private String speakername; /*Palestrante*/
+
+	@Column(name = "date")
+	@Temporal(TemporalType.DATE)
+	private Calendar date;
+
+	@Column(name = "hours")
 	private float hours;
 
-	public void setName(String pName){
-		this.name = pName;
+	protected Activity() {
+
 	}
 	
+	public Activity(String pType, String pTitle, String pDescription, String pSpeakerName, Calendar pDate, float pHours) {
+		this.type = pType;
+		this.title = pTitle;
+		this.description = pDescription;
+		this. speakername = pSpeakerName;
+		this.date = pDate;
+		this.hours = pHours;
+	}
+	
+	public long getID() {
+		return this.id;
+	}
+
+	public void setName(String pSpeakerName){
+		this.speakername = pSpeakerName;
+	}
+
 	public void setType(String pType){
 		this.type = pType;
 	}
@@ -21,15 +72,15 @@ public class Activity {
 	public void setTitle(String pTitle){
 		this.title = pTitle;
 	}
-	
+
 	public void setDescription(String pDescription){
 		this.description = pDescription;
 	}
-	
-	public void setDate(Date pDate){
+
+	public void setDate(Calendar pDate){
 		this.date = pDate;
 	}
-	
+
 	public void setHours(float pHours){
 		this.hours = pHours;
 	}
@@ -37,7 +88,7 @@ public class Activity {
 	public String getType(){
 		return this.type;
 	}
-	
+
 	public String getTitle(){
 		return this.title;
 	}	
@@ -46,11 +97,11 @@ public class Activity {
 		return this.description;
 	}
 
-	public String getName(){
-		return this.name;
+	public String getSpeakerName(){
+		return this.speakername;
 	}
 
-	public Date getDate(){
+	public Calendar getDate(){
 		return this.date;
 	}
 
