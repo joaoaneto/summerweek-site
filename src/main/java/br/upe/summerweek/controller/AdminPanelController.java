9fc73ba584a902c3d5ff9c12f737e24ada0ccf52
silvanoneto <silvano.neto@upe.br>
2017-12-12 02:58:58 -0300

    adicionar atividade e exclusão em controller resolvidos

--- src/main/java/br/upe/summerweek/controller/AdminPanelController.java
+++ src/main/java/br/upe/summerweek/controller/AdminPanelController.java
@@ -1,22 +1,29 @@
 package br.upe.summerweek.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.upe.summerweek.model.Activity;
-import java.util.Date;
+import br.upe.summerweek.model.User;
+import br.upe.summerweek.repository.IActivityRepository;
+import br.upe.summerweek.repository.IUserRepository;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 
 @Controller
-@RequestMapping("/admin/panel")
 public class AdminPanelController {
+	
+	@Autowired
+	IActivityRepository actRep;
+	IUserRepository usrRep;
 	
 	private static final Calendar C = Calendar.getInstance();
 	
@@ -28,9 +35,48 @@ public class AdminPanelController {
 			new Activity("palestra", "Palestra 03-2018", "essa é uma descrição"
 					+ " para a 01", "Matheusa", C, 1.3f)));
 	
-	@GetMapping
-	public String showAdminPanelPage(ModelMap model) {
-		model.addAttribute("activities", TESTE);
+	@GetMapping("/admin/panel")
+	public String showAdminPanelPage(ModelMap model, Activity activity) {
+		model.addAttribute("activities", actRep.findAll());
 		return "adminpanel";
 	}
+	
+	@PostMapping("/activity/insert")
+	public String addNewActivity(@ModelAttribute Activity activity, ModelMap model) {
+		actRep.save(activity);
+		return "redirect:/admin/panel";
+	}
+	
+	@PostMapping("/activity/edit")
+	public String editNewActivity(@ModelAttribute Activity activity, ModelMap model) {
+		actRep.save(activity);
+		return "redirect:/admin/panel";
+	}
+	
+	@GetMapping("/activity/remove")
+	public String removeActivity(@RequestParam("id") String id, ModelMap model) {
+		Long pid = Long.parseLong(id);
+		actRep.deleteById(pid);
+		
+		/*Consertar Null Point Exception*/
+		ArrayList<User>users = (ArrayList<User>)usrRep.findAll();
+		
+		for(int i=0; i<users.size(); i++) {
+			ArrayList<Long> a = users.get(i).getListActivities();
+			
+			for(int j=0; j<a.size(); j++){
+				
+				if(a.get(j) == pid) {
+					a.remove(j);
+					break;
+				}
+			}
+		}
+		
+		usrRep.saveAll(users);
+		
+		return "redirect:/admin/panel";
+	}
+	
+	
 }
